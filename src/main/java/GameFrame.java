90e7aa1684af13fe45b3e6863227e3f776b03aae
KittenPixel <79544605+KittenPixel-cell@users.noreply.github.com>
2022-03-17 16:46:29 -0500

    Update GameFrame.java

--- src/main/java/GameFrame.java
+++ src/main/java/GameFrame.java
@@ -2,14 +2,27 @@ import javax.swing.*;
 
 public class GameFrame  extends JFrame {
 
+    public GamePannel game;
+
+    public static GameFrame getInstance;
+
     GameFrame(){
-        this.add(new GamePannel());
+
+        game = new GamePannel();
+        this.add(game);
         this.setTitle("Kitty Snake V1.0");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
         this.setVisible(true);
         this.setLocationRelativeTo(null);
-
     }
+
+    public void reset()
+    {
+        this.remove(game);
+        this.add(game);
+    }
+
+
 }
